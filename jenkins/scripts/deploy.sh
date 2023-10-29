bd6d190a36ceaafb77b336b0e6c050f78ba5372c
Chow Sow Ying <chowsowying101@gmail.com>
2023-10-29 13:19:29 +0800

    Update URL5

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -x
-docker run -d -p 80:80 --name my-apache-php-app-2 -v /C/Users/chows/Desktop/Jenkins-local/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 8080:80 --name my-apache-php-app-2 -v /C/Users/chows/Desktop/Jenkins-local/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
