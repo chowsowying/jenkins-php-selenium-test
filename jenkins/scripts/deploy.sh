8ad7f82cd5c73bf81a4472ac644e4ccecef5accc
Chow Sow Ying <chowsowying101@gmail.com>
2023-10-29 13:08:03 +0800

    Update URL3

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
-
 set -x
 docker run -d -p 80:80 --name my-apache-php-app -v /C/Users/chows/Desktop/Jenkins-local/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
-echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+# Replace 'localhost' with your laptop's IP address
+laptop_ip="192.168.137.1"
+echo "Now..."
+echo "Visit http://$laptop_ip to see your PHP application in action."
 
