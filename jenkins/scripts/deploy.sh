b099e5b73e78b4a3c9867bca3d499ba8c94c5b41
Chow Sow Ying <chowsowying101@gmail.com>
2023-10-29 13:20:03 +0800

    Update URL6

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -x
-docker run -d -p 8080:80 --name my-apache-php-app-2 -v /C/Users/chows/Desktop/Jenkins-local/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 8080:80 --name my-apache-php-app-3 -v /C/Users/chows/Desktop/Jenkins-local/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
@@ -8,4 +8,5 @@ set +x
 laptop_ip="192.168.137.1"
 echo "Now..."
 echo "Visit http://$laptop_ip to see your PHP application in action."
+
 
